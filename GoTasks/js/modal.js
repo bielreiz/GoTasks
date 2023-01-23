461262b16003e71adc759b9b57652d88c145aaf4
Gabriel Lima <106564641+bielreiz@users.noreply.github.com>
2023-01-23 15:55:51 -0300

    Add files via upload

---
+++ GoTasks/js/modal.js
@@ -0,0 +1,46 @@
+const inputDescription = document.getElementById('description');
+const inputDate = document.getElementById('date');
+const btnCreateTask = document.getElementById('btn-create-task');
+const modal = document.getElementById('modal');
+const alertElement = document.getElementById('alert');
+
+function createTask(e) {
+  e.preventDefault();
+
+  if(!inputDescription.value || !inputDate.value) {
+    alertElement.style.display = 'block';
+    closeAlert();
+    return;
+  }
+
+  const newTask = {
+    description: inputDescription.value,
+    date: new Date(inputDate.value).toLocaleDateString('pt-BR', { timeZone: 'UTC' }),
+    id: Math.floor(Math.random() * 10000)
+  }
+
+  const allTasks = getTasks();
+
+  setTasks([...allTasks, newTask]);
+
+  reload();
+  toggleModal();
+  clearFields();
+}
+
+function toggleModal() {
+  modal.classList.toggle('modal-visible');
+}
+
+function clearFields() {
+  inputDate.value = '';
+  inputDescription.value = '';
+}
+
+function closeAlert() {
+  setTimeout(() => {
+    alertElement.style.display = 'none';
+  }, 3000);
+}
+
+btnCreateTask.addEventListener('click', createTask);
